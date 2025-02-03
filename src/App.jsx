1385f02c9f9cce935736206fc932477b9d607282
EzozaMukhamedova <ezoza.muxamedova@gmail.com>
2025-02-04 08:42:08 +0900

    kahkulator

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import backgroundImage from "./assets/tasbeh3.png";
+import { Position } from "monaco-editor";
 
 function App() {
   let [count, setCount] = useState(0);
@@ -17,14 +19,30 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div
+      className="app"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        width: "500px",
+        height: "500px",
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        margin: "0 auto",
+      }}
+    >
       <p className="count">{count}</p>
-      <button onClick={addCount}>+</button>
-      <button onClick={ayCount}>Ayirish</button>
-      <button onClick={resCount}>Boshidan</button>
+      <button className="add" onClick={addCount}>
+        +
+      </button>
+      <button className="ayrish" onClick={ayCount}>
+        Ay
+      </button>
+      <button className="qoshish" onClick={resCount}>
+        Bo
+      </button>
     </div>
   );
 }
 
 export default App;
-
